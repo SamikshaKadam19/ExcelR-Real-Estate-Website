09f1382f92207f46777094b417b698b838ee1688
Sourav <souravxox@gmail.com>
2024-04-17 15:19:42 +0530

    No one should Touch the Css of Landing page it is completed

--- src/Components/Hero.jsx
+++ src/Components/Hero.jsx
@@ -4,7 +4,6 @@ import React from "react";
 import Navbar from "./Navbar";
 
 import heroImg from "../media/hero_illustration.png";
-import CustomButton from "./CustomButton";
 
 const Hero = () => {
   const CustomBox = styled(Box)(({ theme }) => ({
@@ -19,18 +18,18 @@ const Hero = () => {
     },
   }));
 
-  const Title = styled(Typography)(({ theme }) => ({
-    fontSize: "64px",
-    color: "#000336",
-    fontWeight: "bold",
-    margin: theme.spacing(4, 0, 4, 0),
-    [theme.breakpoints.down("sm")]: {
-      fontSize: "40px",
-    },
-  }));
+    const Title = styled(Typography)(({ theme }) => ({
+      fontSize: "64px",
+      color: "#000336",
+      fontWeight: "bold",
+      margin: theme.spacing(4, 0, 4, 0),
+      [theme.breakpoints.down("sm")]: {
+        fontSize: "40px",
+      },
+    }));
 
   return (
-    <Box sx={{ backgroundColor: "#E6F0FF", minHeight: "80vh" }}>
+    <Box sx={{ backgroundColor: "#E6F0FF", minHeight: "100vh" }}>
       <Container>
         <Navbar />
         <CustomBox>
